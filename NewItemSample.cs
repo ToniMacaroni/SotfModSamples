d1578801957eebda4ab3d22dc3f16bbace42dad8
Julian Kittel <62687014+ToniMacaroni@users.noreply.github.com>
2025-02-12 22:30:52 +0100

    better position in hand and no collision for held prefab

--- NewItemSample.cs
+++ NewItemSample.cs
@@ -29,18 +29,9 @@ public class NewItemSample : SonsMod, IOnAfterSpawnReceiver
 {
     private ItemData _item;
     private GameObject _itemPrefab;
+    private GameObject _heldPrefab;
     private GameObject _pickupPrefab;
     private Texture2D _itemTex;
-    
-    public NewItemSample()
-    {
-        HarmonyPatchAll = true;
-    }
-    
-    protected override void OnInitializeMod()
-    {
-        Config.Init();
-    }
 
     protected override void OnSdkInitialized()
     {
@@ -58,7 +49,13 @@ public class NewItemSample : SonsMod, IOnAfterSpawnReceiver
     {
         var importer = new GLTFSceneImporter(DataPath / "fancy_perfume_bottle.glb", new());
         await importer.LoadSceneAsync();
-        _itemPrefab = importer.CreatedObject.DontDestroyOnLoad().HideAndDontSave();
+        _heldPrefab = importer.CreatedObject.DontDestroyOnLoad().HideAndDontSave();
+        
+        // model is a tad to big so I scale it down
+        _heldPrefab.transform.localScale = Vector3.one * 0.2f;
+
+        // setup itemprefab after heldprefab since it additionally needs colliders on meshrenderers
+        _itemPrefab = _heldPrefab.Instantiate().DontDestroyOnLoad().HideAndDontSave();
         foreach (var renderer in _itemPrefab.GetComponentsInChildren<MeshRenderer>())
         {
             // the renderers need colliders
@@ -70,9 +67,6 @@ public class NewItemSample : SonsMod, IOnAfterSpawnReceiver
             // it doesn't snow in our inventory :)
             renderer.sharedMaterial.SetFloat("_EnableSnow", 0);
         }
-        
-        // model is a tad to big so I scale it down
-        _itemPrefab.transform.localScale = Vector3.one * 0.2f;
     }
     
     /// <summary>
@@ -88,21 +82,16 @@ public class NewItemSample : SonsMod, IOnAfterSpawnReceiver
     private void OnFirstGameActivation()
     {
         _item = ItemTools.CreateAndRegisterItem(667655, "Super Strong Item", description:"Some super strong item.");
-        _item._uiData._icon = _item._uiData._outlineIcon = _itemTex;
+        _item.SetIcon(_itemTex);
         
-        // ============== Some parameters for holdable stuff =============
-        _item._equipmentSlot = EquipmentSlot.RightHand;
-        _item._equippedAnimVars.AddItem(AnimatorVariables.axeHeld);
-        _item._type = Sons.Items.Core.Types.Equippable | Sons.Items.Core.Types.Craftable | Sons.Items.Core.Types.CraftingMaterial;
-        _item._guiType = ItemData.GuiType.Weapon;
-        _item._uiData._leftClick = ItemUiData.LeftClickCommands.equip;
-        // ===============================================================
+        // Some parameters for holdable stuff
+        _item.SetupHeld(EquipmentSlot.RightHand, new [] { AnimatorVariables.molotovHeld });
 
         // use the same model for the pickup
         _pickupPrefab = _itemPrefab;
         
         // also use the same model for the held item
-        _item._heldPrefab = _itemPrefab.transform;
+        _item._heldPrefab = _heldPrefab.transform;
     }
 
     public void OnAfterSpawn()
@@ -114,7 +103,7 @@ public class NewItemSample : SonsMod, IOnAfterSpawnReceiver
             .AddInventoryItem() // add a location in the inventory
             .AddIngredientItem() // add a location on the mat as an ingredient
             .AddCraftingResultItem() // add a location on the mat as a crafting result
-            .SetupHeld() // add a location in the hand. This will enable you to hold the item
+            .SetupHeld(new(0f, 0f, -0.0818f), new(281.3431f, 0f, 0f)) // add a location in the hand. This will enable you to hold the item
             .SetupPickup(_pickupPrefab) // setup a pickup for the item given a model
             .Recipe // recipe setup
             .AddIngredient(ItemTools.Identifiers.Stick, 2) // ingredients of the recipe
